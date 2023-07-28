b03fdb5742437c60837d0ab41b974d5c349af1e9
Reezy2023 <140429405+Reezy2023@users.noreply.github.com>
2023-07-28 18:39:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,13 +10,13 @@ global.SESSION_ID =  process.env.SESSION_ID || "Darknero=NrFGSajS#i-zLanEKF-5jSz
 module.exports = { 
 
  READ_MESSAGE : process.env. READ_MESSAGE  === undefined ?'true': process.env. READ_MESSAGE,
-AOTO_VOICS: process.env.AOTO_VOICS  === undefined ? 'false' : process.env.AOTO_VOICS, 
-YOU_LINK :  process.env.YOU_LINK  === undefined ? 'https://github.com/Kaveeshasithum' : process.env.YOU_LINK,
-OWENR_NO: process.env.OWENR_NO === undefined ? '94774071805' : process.env.OWENR_NO,
-ANTI_BAD: process.env.ANTI_BAD === undefined ? 'false' : process.env.ANTI_BAD,
+AOTO_VOICS: process.env.AOTO_VOICS  === undefined ? 'true' : process.env.AOTO_VOICS, 
+YOU_LINK :  process.env.YOU_LINK  === undefined ? 'https://chat.whatsapp.com/GcJ5iJWGa48LU5TEOwknV4' : process.env.YOU_LINK,
+OWENR_NO: process.env.OWENR_NO === undefined ? '94715652343' : process.env.OWENR_NO,
+ANTI_BAD: process.env.ANTI_BAD === undefined ? 'true' : process.env.ANTI_BAD,
 MAX_SIZE: 100,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-ANTI_LINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
+ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK,
 ALIVE: process.env.ALIVE === undefined ? ` BRO HELO
  
      
@@ -25,5 +25,5 @@ I'm Dark nero Whatsapp user bot
 Helo There I am online 🌐
 
 ᴄʏʙᴇʀ ʏᴀᴋᴜꜱʜᴀ•ᴅᴀʀᴋ ɴᴇʀᴏ-ᴍᴅ` : process.env.ALIVE,
-LOGO: process.env.LOGO === undefined ? `https://i.ibb.co/zrvByTb/IMG-20230609-WA0083.jpg` : process.env.LOGO
+LOGO: process.env.LOGO === undefined ? `https://i.postimg.cc/TwRcJ3Y8/Mine-20230716-131832.jpg` : process.env.LOGO
 };
